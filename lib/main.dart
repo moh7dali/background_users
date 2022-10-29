917e79e273d230bf64d89a643bf646072b41ea0b
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-10-29 18:32:42 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//Nizar's Edits
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
